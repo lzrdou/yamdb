beac5f54db44672212d6434b60de90e3501871ab
Артемий Бутырин <artemii.butyrin@yandex.ru>
2022-07-18 20:00:56 +0300

    created titles app models

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,3 +1,55 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import Avg
+import datetime
 
-# Create your models here.
+
+# Получение текущего года
+def current_year():
+    return datetime.date.today().year
+
+
+# Категория
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Жанр
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Произведение
+class Title(models.Model):
+    name = models.CharField(max_length=512)
+    year = models.PositiveIntegerField(
+        default=current_year(),
+        validators=[MinValueValidator(0), current_year()],
+    )
+    description = models.TextField(max_length=1024)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+    )
+
+    @property
+    def rating(self):
+        return self.reviews.aggregate(avg_score=Avg("score"))["avg_score"]

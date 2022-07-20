324b258e762941dfdda66ff3e0ef38066be47867
Матвей Козлов <kozlovmatvei2002@gmail.com>
2022-07-20 22:43:44 +0300

    Add admin and serializers

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "bio", "role")
+    search_fields = ("username",)
+    list_filter = ("role",)
